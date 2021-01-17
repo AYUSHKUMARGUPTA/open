835fa04828379c84c6d0d6590d041992a2bcd882
Ayush Kumar Gupta <Ayush.Gupta11@gds.ey.com>
2021-01-17 06:15:52 +0530

    Checked in for image link

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -11,27 +11,27 @@ export class SecondComponent implements OnInit {
   constructor() {
     this.cards.push(
       {
-        image: '/assets/Get Paid Instantly.png',
+        image: 'assets/Get Paid Instantly.png',
         title: 'Quick cash disbursement',
         desc: 'Get terms loans that your business needs within 72 hrs'
       },
       {
-        image: '/assets/Low interest rate.png',
+        image: 'assets/Low interest rate.png',
         title: 'Low-interest rate',
         desc: 'Achieve your financial goals with an amazing interest rate starting at 13% per annum'
       },
       {
-        image: '/assets/Secure Payments.png',
+        image: 'assets/Secure Payments.png',
         title: 'Zero Paperwork',
         desc: 'Get started instantly by submitting only your basic details & bank statements'
       },
       {
-        image: '/assets/freelancer_feature_icon.png',
+        image: 'assets/freelancer_feature_icon.png',
         title: 'Ace your business finances',
         desc: 'Manage banking, accounting & everything in between, on one platform'
       },
       {
-        image: '/assets/Covid.png',
+        image: 'assets/Covid.png',
         title: 'Loans to fight COVID-19',
         desc: 'Zero EMI for first 3 months on Back-to-Business loans of upto 1 lakh'
       });
